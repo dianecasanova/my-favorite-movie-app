32d1f7fdd2996a36730d237953c4d47f7f82c438
dianecasanova <festusnova1989@gmail.com>
2020-11-16 15:46:49 +0100

    Creation form to add my favorite movie on the API (id:3049)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FormMovie from "./Components/FormMovie";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <FormMovie />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FormMovie.jsx
@@ -0,0 +1,93 @@
+import { Component } from "react";
+
+class FormMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "", poster: "", comment: "" };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  submitForm = (e) => {
+    e.preventDefault();
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    };
+    const url = "https://post-a-form.herokuapp.com/api/movies";
+
+    fetch(url, config)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error) {
+          alert(res.error);
+        } else {
+          alert(`the movie ${res} has been successfully added!`);
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        alert("There was an error when adding the movie.");
+      });
+  };
+
+  render() {
+    return (
+      <div className="FormMovie">
+        <h1>My favorite movie</h1>
+
+        <form onSubmit={this.submitForm}>
+          <fieldset>
+            <legend>Information</legend>
+            <div className="form-data">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                onChange={this.onChange}
+                value={this.state.title}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="poster">Url</label>
+              <input
+                type="text"
+                id="poster"
+                name="poster"
+                onChange={this.onChange}
+                value={this.state.poster}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="comment">Comment</label>
+              <textarea
+                type="text"
+                id="comment"
+                name="comment"
+                placeholder="why do you like this movie? what made you stand out?"
+                onChange={this.onChange}
+                value={this.state.comment}
+              ></textarea>
+            </div>
+            <hr />
+            <div className="form-data">
+              <input type="submit" value="Send" />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormMovie;
